c36bd1dfd04b3553cf3d15145871596fc606d851
qixiaoshu <2251495182@qq.com>
2022-10-20 10:55:54 +0800

    modify themes

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,7 +1,5 @@
-# name: Jacaetevha
 function tbytes -d 'calculates the total size of the files in the current directory'
   set -l tBytes (ls -al | grep "^-" | awk 'BEGIN {i=0} { i += $5 } END { print i }')
-  
   if test $tBytes -lt 1048576
     set -g total (echo -e "scale=3 \n$tBytes/1048 \nquit" | bc)
     set -g units " Kb"

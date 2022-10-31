96a63b863aab5fa3b025c1b52647fcd3fd33653f
qixiaoshu <2251495182@qq.com>
2022-10-31 09:28:34 +0800

    modify themes

--- config.fish
+++ config.fish
@@ -3,23 +3,23 @@ if status is-interactive
 
 # Colorscheme: Current
 set -U fish_color_normal normal
-set -U fish_color_command 66ccff
-set -U fish_color_autosuggestion b7ae8f
+set -U fish_color_command 66ccf9
 set -U fish_color_quote 00ff87
 set -U fish_color_redirection b48ead
 set -U fish_color_end ff5f5f
 set -U fish_color_error ebcb8b
-set -U fish_color_param 92b3a5
-set -U fish_color_operator 00a6b2
-set -U fish_color_escape 00a6b2
+set -U fish_color_param 66ccff
 set -U fish_color_comment 8cc269
 set -U fish_color_match --background=brblue
 set -U fish_color_selection white --bold --background=brblack
 set -U fish_color_search_match bryellow --background=brblack
 set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
 set -U fish_color_cwd green
 set -U fish_color_cwd_root red
 set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion b7ae8f
 set -U fish_color_user brgreen
 set -U fish_color_host normal
 set -U fish_color_cancel --reverse
--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,32 +1,43 @@
 #welcome
-function holiday -d "weekend"
-set -l week (date '+%A')
-set_color 00b7c3
-if test $week = Monday
-	echo The weekend is 5 day away.
-else if test $week = Tuesday
-	echo The weekend is 4 day away.
-else if test $week = Wednesday
-	echo The weekend is 3 day away.
-else if test $week = Thursday
-	echo The weekend is 2 day away.
-else if test $week = Friday
-	echo The weekend is 1 day away.
+function money -d "payroll" 
+set -l nyr (math (date +%Y)\*10000+(date +%m)\*100+105) 
+set -l temp (math (date -d "$nyr 1500" +%s)/86400-(date +%s)/86400)
+set -l xq (date -d "$nyr" +%u)
+if test $xq=6
+    set final (math -s1 $temp+2)
+else if test $xq=7
+    set final (math -s1 $temp+1)
+else 
+    set final (math -s1 $temp)
+end
+if test $final=1
+    echo $final day away from payroll.
 else
-	echo Today is the weekend.
+    echo $final days away from payroll. 
+end
+end
+
+function holiday -d "weekend"
+set -l wday (math 5-(date +%u))
+set_color 00b7c3
+if test $wday -ge 0
+    echo The weekend is $wday day away.
+else
+    echo Today is the weekend.
 end
 end
 
 function runnian -d "ruannian"
-set -l year (date '+%Y')
-set -l day (date '+%j')
-set -l run (math 366-$day)
-set -l ping (math 365-$day)
+set -l ny (math (date +%Y)\*10000+10101)
+set -l temp (math -s0 (date -d "$ny" +%s)/86400-(date +%s)/86400+1)
+
 set_color 00b7c3
-if test $year%4=0 -a $year%100!=0 -o $year%400=0
-	echo {$run} days left this year.
+if test $temp -eq 1
+    echo Countdown one day !!!.
+else if test $temp -eq 0
+    echo Happy new year !!!!!!
 else 
-	echo {$ping} days left this year.
+    echo $temp days left this year.
 end
 end
 
@@ -35,3 +46,4 @@ date '+Today is %F %A.'
 date '+It is the %jth day and %Wth week of this year.'
 runnian
 holiday
+money
